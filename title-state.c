c025e844cb2dc50961fb40a6972a2b45b1874d7c
Kyle Schreiber <admin@mopiz.com>
2012-08-30 21:43:34 -0400

    Added collision detection of title-state.c for the play and options buttons.
    The states to change when collision is detected have not been created. Currently, it is just a printf.
    
    Also consider implementing a hover color so that the user knows what thing they are clicking on.

--- title-state.c
+++ title-state.c
@@ -17,6 +17,10 @@ int l2TitleY = -250;
 int wordY = 150;
 int fallY = 200;
 
+SDL_Rect playRect;
+SDL_Rect optionsRect;
+
+
 void title_events()
 {
 	//printf("title\n");
@@ -27,15 +31,30 @@ void title_events()
 		else if(event.key.keysym.sym == SDLK_ESCAPE) {
 			set_next_state(STATE_EXIT);
 		}
-		
-		// collision detection for play and options buttons
+		else if(event.type == SDL_MOUSEBUTTONDOWN) {
+			if(event.button.button == SDL_BUTTON_LEFT) {
+				if((event.motion.x > playRect.x) &&
+					(event.motion.x < playRect.x + playRect.w) &&
+					(event.motion.y > playRect.y) &&
+					(event.motion.y < playRect.y + playRect.h)) {
+					// set_next_state(STATE_PLAY);
+					printf("Play button clicked\n");
+				}
+				else if((event.motion.x > optionsRect.x) &&
+					(event.motion.x < optionsRect.x + optionsRect.w) &&
+					(event.motion.y > optionsRect.y) &&
+					(event.motion.y < optionsRect.y + optionsRect.h)) {
+					// set_next_state(STATE_OPTIONS);
+					printf("options button clicked\n");
+				}
+			}
+		}
 	}
 }
 
 void title_logic()
 {
-	// This needs to be cleaned up with an array or something
-	// I don't know what this does...
+	// The code below is atrocious but I don't know how to fix it.
 	if(wTitleY < wordY)
 		wTitleY += 5;
 	if(wTitleY > wordY)
@@ -75,6 +94,7 @@ void title_logic()
 		l2TitleY += 5;
 	if(l2TitleY > fallY)
 		l2TitleY = fallY;
+
 }
 
 void title_render()
@@ -98,12 +118,23 @@ void title_render()
 	render_image(0,560,grass,screen);
 	
 	SDL_Color playColor = {0,0,0};
+	SDL_Color hoverColor = {254,210,6};
 	play = render_font(playFont, "Play", playColor);
 	render_image(148, 300, play, screen);
-	
 	options = render_font(optionsFont, "Options", playColor);
 	render_image(115, 350, options, screen);
 	
+	
+	// Collision rects for play and option buttons
+	playRect.x = 148;
+	playRect.y = 300;
+	playRect.w = play->clip_rect.w;
+	playRect.h = play->clip_rect.h;
+	optionsRect.x = 115;
+	optionsRect.y = 350;
+	optionsRect.w = options->clip_rect.w;
+	optionsRect.h = options->clip_rect.h;
+
 	if(SDL_Flip(screen) != 0) {
 		fprintf(stderr, "screen update failed\n");
 	}
